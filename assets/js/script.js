9839f37496d4cd0f52926a67a0e56d828c74caee
josik <joe.code.mckinney@gmail.com>
2023-04-15 10:47:17 -0500

    added date into header, colored text area based on time, added and tested local storage save functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,54 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
+
+// Wrap all code that interacts with the DOM in a call to jQuery 
+$(document).ready(function() {
+  
+  // *Add a listener for click events on the save button. 
+  $(".saveBtn").on("click", function() {
+    var btnNum = $(this).parent().attr("id");
+    var textInside = $(this).siblings('textarea').val();
+    localStorage.setItem(btnNum, textInside);
+  });
+
+  // * Add code to display the current date in the header of the page.
+  const now = dayjs();
+  $('#currentDay').text(now.format('dddd MMMM D, YYYY'));
+
+  // * Add code to apply the past, present, or future class to each time
+  milTime = now.format('HH')
+  console.log('mil time is ' + milTime + typeof milTime)
+  //for each in jquery
+   
+  
+  
+  $("div[id^='hour'").each(function() {
+    // console.log the array created by split;
+    const planHour = this.id.split('-');
+    // console.log(planHour)
+    console.log(parseInt(milTime), parseInt(planHour[1]))
+    if (parseInt(milTime) === parseInt(planHour[1])){
+      $(this).addClass('present')
+    } else if (parseInt(milTime) > parseInt(planHour[1])){
+      $(this).addClass('past')
+    } else {
+      $(this).addClass('future')
+    }
+  })
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
+  //loop through local storage
+  for (var i= 0; i < localStorage.length; i++){
+  const key = localStorage.key(i);
+  if (key.startsWith('hour')){
+   const splitKey = key.split('-')
+   keyNum = parseInt(splitKey[1])
+   console.log(keyNum)
+  }};
+
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
+  });
 
 // psudo code from class
 //  Calendar App
