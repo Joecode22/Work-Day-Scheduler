91585f8f45ee154dd0d88e00beab06c79ed65cfa
josik <joe.code.mckinney@gmail.com>
2023-04-15 13:24:24 -0500

    clean-up, removed console logs, added comments that describe functionality of each section  as an active recall excercise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
-
+//Challenge Assignment Module 05
+//Work Day Scheduler
+//Joseph McKinney
+//04/15/2023
 
 // Wrap all code that interacts with the DOM in a call to jQuery 
 $(document).ready(function() {
-  
-  // *Add a listener for click events on the save button. 
+  // *Add a listener for click events on the save button.
+  // grabs the id of the parent element
+  // grabs the text content of the sibling element
+  // stores these two things into local storage
   $(".saveBtn").on("click", function() {
     var btnNum = $(this).parent().attr("id");
     var textInside = $(this).siblings('textarea').val();
@@ -11,21 +16,22 @@ $(document).ready(function() {
   });
 
   // * Add code to display the current date in the header of the page.
+  // creates the variable now and assigns it the output of the dayjs() query
   const now = dayjs();
+  // jquery code for find the element with an id of 'currentDay'
+  // and then go get the current date using dayjs
+  // and set it as the text content of the requested element
   $('#currentDay').text(now.format('dddd MMMM D, YYYY'));
 
   // * Add code to apply the past, present, or future class to each time
   milTime = now.format('HH')
   console.log('mil time is ' + milTime + typeof milTime)
   //for each in jquery
-   
-  
-  
   $("div[id^='hour'").each(function() {
-    // console.log the array created by split;
+    //grab the id, then split it into an array using '-' as the divisor
     const planHour = this.id.split('-');
-    // console.log(planHour)
-    console.log(parseInt(milTime), parseInt(planHour[1]))
+    // compares the div's hour with the 'HH' formatted output of the dayjs
+    // adds appropriate class based on past, present, or future
     if (parseInt(milTime) === parseInt(planHour[1])){
       $(this).addClass('present')
     } else if (parseInt(milTime) > parseInt(planHour[1])){
@@ -35,10 +41,17 @@ $(document).ready(function() {
     }
   })
   
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  //loop through local storage
+  // * Gets items from local storage and loads the text area of the divs that match the appropriate hour
+  //for loop in JavaScript to loop through the items in local storage
   for (var i= 0; i < localStorage.length; i++){
+    //grab the key
     const key = localStorage.key(i);
+    //check to see if it is the key we want
+    //split the key into an array with the '-' as the divisor
+    //compare the key with the div id 
+    //and if they match, get the value from local storage
+    //go find an element of type textarea
+    //write the value in local storage to the text area  
     if (key.startsWith('hour')){
     const splitKey = key.split('-')
     keyNum = parseInt(splitKey[1])
@@ -47,30 +60,8 @@ $(document).ready(function() {
       if (planHour == keyNum) {
       console.log(planHour, keyNum, 'ding!')
       console.log(localStorage.getItem(key))
-      console.log($(this).find('textarea').val(localStorage.getItem(key)));
+      $(this).find('textarea').val(localStorage.getItem(key));
       }
     });
   }};
 });
-
-
-
-
-
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-// psudo code from class
-//  Calendar App
-
-// Read from localStrorage
-//  Populate page with data if exists in localStorage
-
-// click event with delegation to target the text area assoc with the save button
-// use the sibling DOM method to traverse the DOM, event.target parent/sibling
-//  capture the value of the text area, save string to local storage 
-
-// Use Dayjs to get current time use military time
-// read the value of the time block from the html
-// if statement that compares the vales and adds a class to change the color
-// loop through html elements and read a data-attribute time
